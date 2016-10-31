002d1f1429e4750ce3986ae55007e641d4e1d8c5
Dimitri Theulings <dimitri@tastypies.co.uk>
2016-10-31 12:32:46 +0000

    Upgrade to Play 2.5.x

--- app/uk/gov/hmrc/submissiontracker/binder/Binders.scala
+++ app/uk/gov/hmrc/submissiontracker/binder/Binders.scala
@@ -16,11 +16,20 @@
 
 package uk.gov.hmrc.submissiontracker.binder
 
+import play.api.mvc.PathBindable
 import uk.gov.hmrc.domain.Nino
-import uk.gov.hmrc.play.binders.SimpleObjectBinder
-
-object NinoBinder extends SimpleObjectBinder[Nino](Nino.apply, _.value)
 
 object Binders {
-  implicit val ninoBinder = NinoBinder
+
+  implicit def ninoBinder(implicit stringBinder: PathBindable[String]) = new PathBindable[Nino] {
+
+    def unbind(key: String, nino: Nino): String = stringBinder.unbind(key, nino.value)
+
+    def bind(key: String, value: String): Either[String, Nino] = {
+      Nino.isValid(value) match {
+        case true => Right(Nino(value))
+        case false => Left("ERROR_NINO_INVALID")
+      }
+    }
+  }
 }
