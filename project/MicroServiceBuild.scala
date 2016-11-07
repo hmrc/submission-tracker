aca37c53cbbe81491b234721f3020866aeab20e4
Dimitri Theulings <dimitri@tastypies.co.uk>
2016-11-07 12:13:28 +0000

    Fix logging issues.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -21,11 +21,10 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val microserviceBootstrapVersion = "5.4.0"
+  private val microserviceBootstrapVersion = "5.6.0"
   private val playAuthVersion = "4.2.0"
   private val playHealthVersion = "2.0.0"
-  private val hmrcPlayJsonLoggerVersion = "2.1.1"
-  private val logbackJsonLoggerVersion = "3.0.0"
+  private val playJsonLoggerVersion = "3.0.0"
   private val playUrlBindersVersion = "2.0.0"
   private val playConfigVersion = "3.0.0"
   private val domainVersion = "4.0.0"
@@ -46,7 +45,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
     "uk.gov.hmrc" %% "reactive-circuit-breaker" % "1.7.0",
     "uk.gov.hmrc" %% "emailaddress" % "1.1.0"
