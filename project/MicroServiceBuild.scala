b20a428d288fa89d70de98fa90fc31fa09a95cca
HerbiePorter <herbieporter@gmail.com>
2017-05-02 09:48:16 +0100

    NGC-1471 Upgraded bootstrap for the .mdtp domain changes.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -23,7 +23,7 @@ private object AppDependencies {
 
   private val logbackJsonLoggerVersion = "3.1.0"
 
-  private val microserviceBootstrapVersion = "5.14.0"
+  private val microserviceBootstrapVersion = "5.15.0"
   private val playAuthVersion = "4.3.0"
   private val playHealthVersion = "2.1.0"
   private val playUrlBindersVersion = "2.1.0"
